97a25f2d5619ae5e463bd43d1e5ba540c35dc97a
mac linux ubnu <1018367@qq.com>
2017-01-10 23:43:23 +0800

    电脑端完成,待移植到手机端

--- hl.py
+++ hl.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
+#coding:utf-8
+import requests
+import time
+from colorama import init, Fore
 
+while True:
+	time.sleep(2)
+	url_cn = 'https://www.okcoin.cn/api/v1/ticker.do?symbol=btc_cny'
+	url_int = 'https://www.okcoin.com/api/v1/ticker.do?symbol=btc_usd'
+	r = requests.get(url_cn)
+	l = requests.get(url_int)
+	last_price_cn = r.json()['ticker']['last']
+	last_price_int = l.json()['ticker']['last']
+	hl = float(last_price_cn)/float(last_price_int)
+	print('币价: |', Fore.RED + str(last_price_cn) + Fore.RESET,'¥',Fore.RED + str(last_price_int) + Fore.RESET,'$')
+	print('汇率: |', Fore.GREEN + str(hl) + Fore.RESET, '\n--------------------------')
